9718407b987b3aff5aede6f3a87bed30eca7a5dc
“Joseph <joe.timmer89@gmail.com>
2014-10-16 18:28:06 -0500

    refactored meetings controller, removed logic from controller and moved it to model for formatting google calendar api hash

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,27 +4,10 @@ class MeetingsController < ApplicationController
   end
 
   def create
-  	@event = Meeting.new(title: params[:meeting][:title])
+  	@event = Meeting.new(params[:meeting])
   	respond_to do |format|
   		if @event.save
-  			event = {
-  				'summary' => @event.title,
-  				'location' => 'Chinatown',
-  				'description' => "It will be so cooool!",
-  				'source' => { 
-  					'title' => "Jimmy Joe", 
-  					'url' => "http://jtimmer89.github.io"
-  				},
-  				'start' => {
-  					'dateTime' => '2014-11-10T10:00:00.000-07:00',
-  				},
-  				'end' => {
-  					'dateTime' => '2014-11-11T11:00:00.000-07:00'
-  				},
-  				'attendees' => [ { 'email' => 'isaacnoda@gmail.com' } ]
-  			}
-
-  			current_user.create_event(event)
+  			current_user.create_event(Meeting.event_hash(@event))
 
   			format.html { redirect_to root_path, notice: 'Event was successfully created.' }
   			format.json { render json: @event, status: :created, location: @event }
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,4 +7,22 @@ class Meeting < ActiveRecord::Base
   has_many :actionables
 
   has_many :agenda_topics
+
+  def self.event_hash(event)
+  	{ 'summary' => event.title,
+			'location' => event.location,
+			'description' => event.invite_notes,
+			'start' => { 'dateTime' => event.start_time, 'timeZone' => event.timezone },
+			'end' => { 'dateTime' => event.end_time },
+			'attendees' => event.invitees_array
+		}
+  end
+
+  def invite_notes
+  	"#{self.description} \nlocalhost:3000/meetings/#{self.id}"
+  end
+
+  def invitees_array
+  	self.invitees.map { |invitee| emails_array << { 'email' => invitee.email } }
+  end
 end
