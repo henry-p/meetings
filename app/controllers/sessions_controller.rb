e5d566731f819d9b9714a26707f8a65baab7ad4d
Isaac Noda <isaacnoda@gmail.com>
2014-10-16 12:58:39 -0500

    began refactoring controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,17 +12,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    require 'net/http'
-
-
     user_info = request.env["omniauth.auth"]['info']
     user_credentials = request.env["omniauth.auth"]['credentials']
     user = User.find_or_create_by(email: user_info['email'])
-    user.update(image_path: user_info['image'], first_name: user_info['first_name'], last_name: user_info['last_name'])
-
+    user.update(image_path: user_info['image'], first_name: user_info['first_name'], last_name: user_info['last_name'], token: user_credentials['token'])
 
     client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :site => 'https://www.google.com')
     token = OAuth2::AccessToken.new(client, user_credentials['token'])
+
+    user.load_contacts
 
     google_contacts_user = GoogleContactsApi::User.new(token)
     puts google_contacts_user.contacts.first
@@ -32,11 +30,9 @@ class SessionsController < ApplicationController
 
     # google_contacts_user.contacts.first.full_name
     # google_contacts_user.contacts.first.pirmary_email
-    raise
   
 
     session[:user_id] = user.id
-    session[:token] = user_credentials['token']
 
     redirect_to profile_path
   end
