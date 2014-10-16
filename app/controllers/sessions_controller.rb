0007d866900b00c0aba0d9fae3ef46717cd6eb47
Isaac Noda <isaacnoda@gmail.com>
2014-10-16 14:45:01 -0500

    refactored google_api_client gem implementation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,23 +14,11 @@ class SessionsController < ApplicationController
   def create
     user_info = request.env["omniauth.auth"]['info']
     user_credentials = request.env["omniauth.auth"]['credentials']
+
     user = User.find_or_create_by(email: user_info['email'])
     user.update(image_path: user_info['image'], first_name: user_info['first_name'], last_name: user_info['last_name'], token: user_credentials['token'])
 
-    client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :site => 'https://www.google.com')
-    token = OAuth2::AccessToken.new(client, user_credentials['token'])
-
     user.load_contacts
-
-    google_contacts_user = GoogleContactsApi::User.new(token)
-    puts google_contacts_user.contacts.first
-
-    # google_contacts_user.contacts.first.family_name LAST NAME
-    # google_contacts_user.contacts.first.given_name FIRST NAME
-
-    # google_contacts_user.contacts.first.full_name
-    # google_contacts_user.contacts.first.pirmary_email
-  
 
     session[:user_id] = user.id
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,61 @@ class User < ActiveRecord::Base
   has_many :meetings, foreign_key: :creator_id
 
   has_many :agenda_topics, foreign_key: :creator_id
+
+  def google_client
+    client = Google::APIClient.new
+    client.authorization.access_token = self.token
+    client
+  end
+
+  def calendar_service
+    self.google_client.discovered_api('calendar', 'v3')
+  end
+
+  
+
+  def contacts_service
+    self.google_client.discovered_api('calendar', 'v3')
+  end
+
+  def load_contacts
+    google_contacts_user = GoogleContactsApi::User.new(self.oauth2_token_object)
+
+
+    contact_data = google_contacts_user.contacts.map do |contact|
+      { full_name: contact.full_name, emails: contact.emails }
+    end.to_json
+
+    $redis.set("#{self.id}", contact_data)
+  end
+
+
+  def oauth2_token_object 
+    OAuth2::AccessToken.new(self.google_client, self.token)
+  end
 end
+
+
+
+
+# event = {
+#           'summary' => @event.title,
+#           'location' => 'Chinatown',
+#           'start' => {
+#             'dateTime' => '2011-06-03T10:00:00.000-07:00'
+#           },
+#           'end' => {
+#             'dateTime' => '2014-11-11T11:00:00.000-07:00'
+#           },
+#           'attendess' => [ { 'email' => 'joe.timmer@loringindustries.com' } ]
+#         }
+#         # define user token here
+
+
+        # client = Google::APIClient.new
+        # client.authorization.access_token = self.token
+#         service = client.discovered_api('calendar', 'v3')
+#         result = client.execute(:api_method => service.events.insert,
+#                                 :parameters => {'calendarId' => 'primary' },
+#                                 :body => JSON.dump(event),
+#                                 :headers => { 'Content-Type' => 'application/json' } )
