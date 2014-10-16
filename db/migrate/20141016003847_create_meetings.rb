c583d94f82e03e4d63c240694581c6a6e3670ecd
“Joseph <joe.timmer89@gmail.com>
2014-10-16 18:57:33 -0500

    added description to meetings

--- db/migrate/20141016003847_create_meetings.rb
+++ db/migrate/20141016003847_create_meetings.rb
@@ -3,6 +3,7 @@ class CreateMeetings < ActiveRecord::Migration
     create_table :meetings do |t|
       t.integer :creator_id
       t.string :title
+      t.string :description
       t.string :location
       t.datetime :start_time
       t.datetime :end_time
