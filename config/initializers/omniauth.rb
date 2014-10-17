f80247e6033d41bb82c481be1cf6a782ed7a3733
Isaac Noda <isaacnoda@gmail.com>
2014-10-16 21:02:29 -0500

    added error handling if user denies access permission

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,4 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     scope:['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/contacts.readonly', 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.profile'],
     redirect_uri:ENV['REDIRECT_URI']
   }
+
+  OmniAuth.config.on_failure = UsersController.action(:show)
 end
