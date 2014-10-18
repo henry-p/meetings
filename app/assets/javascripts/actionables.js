4220cf26b16bb684d241bd8a609292484fffd231
Isaac Noda <isaacnoda@gmail.com>
2014-10-17 20:12:34 -0500

    agenda topics are deleteable

--- app/assets/javascripts/actionables.js
+++ app/assets/javascripts/actionables.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
 $("body").on("click", "#actionable_button", function() {
   alert("you win")
 });
+
+  $("body").on('focusout', '.agenda .editable', function() {
+    var agendaId = $(this).parent().attr('id')
+    var agendaContent = $(this).context.innerText
+    $.ajax({
+
+    });
+  });
 });
--- app/controllers/agenda_topics_controller.rb
+++ app/controllers/agenda_topics_controller.rb
@@ -22,4 +22,16 @@ class AgendaTopicsController < ApplicationController
       format.js
     end  
   end
+
+  def destroy
+    @meeting = Meeting.find_by_id(params[:meeting_id])
+
+    @agenda_topic = AgendaTopic.find_by_id(params[:id])
+    @agenda_topic.destroy
+
+    respond_to do |format|
+      format.html { redirect_to meeting_path(@meeting) }
+      format.js
+    end      
+  end
 end
