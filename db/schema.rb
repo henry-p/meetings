ed7d94f70f699a73812dfbdc0cc85b60aba4b291
henry-p <henry.perschk@gmail.com>
2014-10-18 16:42:54 -0500

    git-ignored dump.rdb, commented out seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141018204548) do
-=======
-ActiveRecord::Schema.define(version: 20141018180640) do
->>>>>>> development
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,10 +95,7 @@ ActiveRecord::Schema.define(version: 20141018180640) do
     t.datetime "updated_at"
     t.string   "token"
     t.string   "refresh_token"
-<<<<<<< HEAD
     t.integer  "token_expires_at"
-=======
->>>>>>> development
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
