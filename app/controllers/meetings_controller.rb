da5611e272ccbe0439a42623be91fa2470086e0e
“Joseph <joe.timmer89@gmail.com>
2014-10-19 12:22:01 -0500

    fixed delete meeting if google event create fails

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,122 +1,123 @@
 class MeetingsController < ApplicationController
-  skip_before_action :require_login, only: [:show, :check_invited]
-  before_filter(only: [:update, :destroy]) { |filter| filter.check_if_meeting_is_closed(params[:id]) }
+	skip_before_action :require_login, only: [:show, :check_invited]
+	before_filter(only: [:update, :destroy]) { |filter| filter.check_if_meeting_is_closed(params[:id]) }
 
-  def new
-    @meeting = Meeting.new
-  end
+	def new
+		@meeting = Meeting.new
+	end
 
-  def contacts
-    contacts = $redis.get(current_user.id)
-    respond_to do |format|
-      format.json { render json: contacts }
-    end
-  end
+	def contacts
+		contacts = $redis.get(current_user.id)
+		respond_to do |format|
+			format.json { render json: contacts }
+		end
+	end
 
-  def show
-    @meeting = Meeting.find_by_id(params[:id])
-    if logged_in?
-      if current_user != @meeting.creator
-        unless @meeting.invitees.include?(current_user)
-          flash[:error] = "You don't have access to this meeting."
-          redirect_to profile_path
-        end
-      end
-    else
-      render :invited
-    end
-  end
+	def show
+		@meeting = Meeting.find_by_id(params[:id])
+		if logged_in?
+			if current_user != @meeting.creator
+				unless @meeting.invitees.include?(current_user)
+					flash[:error] = "You don't have access to this meeting."
+					redirect_to profile_path
+				end
+			end
+		else
+			render :invited
+		end
+	end
 
-  def check_invited
-    @meeting = Meeting.find_by_id(params[:meeting_id])
-    @invitee_emails = @meeting.invitees.pluck(:email)
+	def check_invited
+		@meeting = Meeting.find_by_id(params[:meeting_id])
+		@invitee_emails = @meeting.invitees.pluck(:email)
 
-    if @invitee_emails.include?(params[:email])
-      session[:user_id] = User.find_by_email(params[:email]).id
-      render :show
-    else
-      flash[:error] = "This email address is not on the list of the people invited to this meeting."
-      render :invited
-    end
-  end
+		if @invitee_emails.include?(params[:email])
+			session[:user_id] = User.find_by_email(params[:email]).id
+			render :show
+		else
+			flash[:error] = "This email address is not on the list of the people invited to this meeting."
+			render :invited
+		end
+	end
 
-  def create
-  	if Meeting.empty_datetime(params)
-  		@meeting = Meeting.new(meeting_params_without_time)
-  		flash.now[:error] = "Please give a start and end time to create an event."
-  		render :new
-  	elsif Meeting.empty_title(params)
-  		@meeting = Meeting.new(meeting_params)
-  		flash.now[:error] = "Please include a title to create an event."
-  		render :new
-  	else
-	  	@meeting = Meeting.new(meeting_params.merge(creator: current_user))
-      if @meeting.save
-  	  	Invite.create_invites(params[:attendees], @meeting)
-      end
-      
+	def create
+		if Meeting.empty_datetime(params)
+			@meeting = Meeting.new(meeting_params_without_time)
+			flash.now[:error] = "Please give a start and end time to create an event."
+			render :new
+		elsif Meeting.empty_title(params)
+			@meeting = Meeting.new(meeting_params)
+			flash.now[:error] = "Please include a title to create an event."
+			render :new
+		else
+			@meeting = Meeting.new(meeting_params.merge(creator: current_user))
+			if @meeting.save
+				Invite.create_invites(params[:attendees], @meeting)
+			end
+			
 			response = current_user.create_event(Meeting.event_hash(@meeting))
 			if response.status == 200
 				@meeting.update(calendar_event_id: response.data.id)
 				redirect_to root_path, flash: { success: "Your event was successfully created." }
 			else
+				@meeting.destroy
 				@meeting.invites.destroy_all
 				flash.now[:error] = "Google was not able to create your event. Please try again."
 				render :new
 			end
 		end
-  end
+	end
 
-  def edit
-  	@meeting = Meeting.find_by_id(params[:id])
-  end
+	def edit
+		@meeting = Meeting.find_by_id(params[:id])
+	end
 
-  def update
-    if params[:close]
-      @meeting = Meeting.find_by_id(params[:id]) 
-      @meeting.update(is_done: true)
-      return redirect_to root_path
-    end
+	def update
+		if params[:close]
+			@meeting = Meeting.find_by_id(params[:id]) 
+			@meeting.update(is_done: true)
+			return redirect_to root_path
+		end
 
 
-  	if Meeting.empty_datetime(params)
-  		@meeting = Meeting.new(meeting_params_without_time)
-  		flash.now[:error] = "Please give a start and end time to edit an event."
-  		render :edit
-  	elsif Meeting.empty_title(params)
-  		@meeting = Meeting.new(meeting_params)
-  		flash.now[:error] = "Please include a title to edit an event."
-  		render :edit
-  	else
-	  	@meeting = Meeting.find_by_id(params[:id])
-	  	@meeting.invites.destroy_all
-	  	Invite.create_invites(params[:attendees], @meeting)
-	  	unsaved_event = @meeting.clone
-	  	response = current_user.update_event(Meeting.event_hash(unsaved_event), @meeting.calendar_event_id)
-	  	if response.status == 200
-	  		@meeting.update(meeting_params)
-	  		redirect_to root_path, flash: { success: "Your event was successfully edited." }
-	  	else
-	  		@meeting.invites.destroy_all
+		if Meeting.empty_datetime(params)
+			@meeting = Meeting.new(meeting_params_without_time)
+			flash.now[:error] = "Please give a start and end time to edit an event."
+			render :edit
+		elsif Meeting.empty_title(params)
+			@meeting = Meeting.new(meeting_params)
+			flash.now[:error] = "Please include a title to edit an event."
+			render :edit
+		else
+			@meeting = Meeting.find_by_id(params[:id])
+			@meeting.invites.destroy_all
+			Invite.create_invites(params[:attendees], @meeting)
+			unsaved_event = @meeting.clone
+			response = current_user.update_event(Meeting.event_hash(unsaved_event), @meeting.calendar_event_id)
+			if response.status == 200
+				@meeting.update(meeting_params)
+				redirect_to root_path, flash: { success: "Your event was successfully edited." }
+			else
+				@meeting.invites.destroy_all
 				flash.now[:error] = "Google was not able to update your event. Please try again."
 				render :edit
-	  	end
-	  end
-  end
+			end
+		end
+	end
 
-  def destroy
-  	@meeting = Meeting.find_by_id(params[:id])
-  	response = current_user.delete_event(@meeting.calendar_event_id)
-  	if response.status == 204
-  		@meeting.destroy 
-  		redirect_to root_path, flash: { success: "Your event was successfully deleted." }
-  	else
-  		redirect_to root_path, flash: { error: "Google was not able to delete your event. Please try again." }
-  	end
-  end
+	def destroy
+		@meeting = Meeting.find_by_id(params[:id])
+		response = current_user.delete_event(@meeting.calendar_event_id)
+		if response.status == 204
+			@meeting.destroy 
+			redirect_to root_path, flash: { success: "Your event was successfully deleted." }
+		else
+			redirect_to root_path, flash: { error: "Google was not able to delete your event. Please try again." }
+		end
+	end
 
-  private
-  
+	private
+	
 	def meeting_params
 		Meeting.format_params(params.require(:meeting).permit(:title, :description, :location, :start_time, :end_time, :time_zone, :notes))
 	end
