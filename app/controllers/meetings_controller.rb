b129e5ab4340d6a8eea4a9773233b2e3ead0eec4
henry-p <henry.perschk@gmail.com>
2014-10-20 11:52:31 -0500

    working

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -11,7 +11,10 @@ class MeetingsController < ApplicationController
 
 			@meeting = previous_meeting.dup
 			@meeting.invitees = previous_meeting.invitees
-			@meeting.update(start_time: local_time, end_time: local_time + previous_meeting.duration_in_seconds)
+			day_after_meeting = previous_meeting.start_time + 86400
+			start_time = day_after_meeting < local_time ? local_time : day_after_meeting
+			end_time = start_time + previous_meeting.duration_in_seconds
+			@meeting.update(start_time: start_time, end_time: end_time)
 		else
 			@meeting = Meeting.new
 		end
