63dcb75546da1407b95c0660dde5c4f96d890a14
sspread <sspread2@gmail.com>
2014-10-20 00:36:50 -0500

    Added delete and unique features to dragon drop

--- app/assets/javascripts/dragon_drop.js
+++ app/assets/javascripts/dragon_drop.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  $('.dragon-drop').draggable({helper: "clone"});
+  $('#invitees .dragon-drop').draggable({helper: "clone"});
 
   $('body').on('dragstart', '.dragon-drop', function() {
     $('.actionable').droppable({
       drop: function( event, ui ) {
+        $('.draggable').draggable('disabled');
         var meetingId = $('#meeting-id').text();
         var draggable = ui.draggable.clone();
         var draggableId = draggable.attr('id');
-        console.log(draggableId)
         var actionableId = $(event.target).attr('id');
-        console.log(actionableId)
         $.ajax({
           url: "/meetings/"+meetingId+"/actionables/"+actionableId+"/responsibilities",
           type: "POST",
--- app/controllers/responsibilities_controller.rb
+++ app/controllers/responsibilities_controller.rb
@@ -1,10 +1,24 @@
 class ResponsibilitiesController < ApplicationController
+  before_filter :find_goodies
 
   def create
-    @actionable = Actionable.find_by_id(params[:actionable_id])
-    @meeting = @actionable.meeting
     @user = User.find_by_id(params[:user_id])
-    @responsibility = Responsibility.create(user_id: params[:user_id], actionable_id: params[:actionable_id])
+    @responsibility = Responsibility.new(user_id: params[:user_id], actionable_id: @actionable.id)
+    unless @responsibility.save
+      redirect_to meeting_path(@meeting)
+    end
   end
 
+  def destroy
+    @responsibility = Responsibility.find_by_id(params[:id])
+    @user = User.find_by_id(@responsibility.user_id)
+    @responsibility.destroy
+  end
+
+  private
+
+  def find_goodies
+    @actionable = Actionable.find_by_id(params[:actionable_id])
+    @meeting = @actionable.meeting
+  end
 end
