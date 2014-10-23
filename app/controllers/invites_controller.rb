cd635a235f7becb8fef2796ced76eac8d4e5be8b
sspread <sspread2@gmail.com>
2014-10-23 10:58:51 -0500

    Changed datetime display and fixed heading title on meeting pre-start page

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,7 +4,7 @@ class InvitesController < ApplicationController
 		response = current_user.delete_event(@meeting.calendar_event_id)
 		if google_api_call_success?(response) || google_api_call_404(response)
 			Invite.find_by_id(params[:id]).destroy
-			redirect_to root_path, flash: { success: "Your successfully removed yourself from the event." }
+			redirect_to root_path, flash: { success: "You successfully removed yourself from the event." }
 		else
 			redirect_to root_path, flash: { error: "Google was not able to delete you from the event. Please try again." }
 		end
